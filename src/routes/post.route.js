6e9967e5f7b11a3f1078c7554c7b33932df640cb
Long5678 <110109135+Long5678@users.noreply.github.com>
2024-03-22 11:56:27 +0700

    as

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -3,7 +3,9 @@ import postController from "../app/controllers/PostController.js";  // step 3
 
 const router = express.Router(); // step 2
 
-router.use('/:id', postController.show); // localhost:3000/posts/101
-router.use('/', postController.index); // localhost:3000/posts/
+router.post('/store', postController.store); // localhost:3000/posts/store
+router.get('/create', postController.create); // localhost:3000/posts/create
+router.get('/:id', postController.detail); // localhost:3000/posts/101
+router.get('/', postController.index); // localhost:3000/posts/
 
 export default router;
